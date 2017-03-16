51cd9da10777104af5672c90a0bfee4a60111ec5
KanYousaf <kanwal.yousaf@uettaxila.edu.pk>
2017-03-16 11:25:09 +0500

    My Initial Commit after adding alert dialog and Shared Preferences

--- app/src/main/java/com/example/admin/lecture06/MainActivity.java
+++ app/src/main/java/com/example/admin/lecture06/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.admin.lecture06;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -50,14 +55,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myFavSeasonList = (ListView) this.findViewById(R.id.list_of_seasons);
-
-        //put data of arrays into arraylists
-        season_names_AL=new ArrayList<>(Arrays.asList(myFavSeasonArray));
-        season_images_AL=new ArrayList<>();
-        for(int i=0; i< myFavSeasonImageArray.length; i++){
-            season_images_AL.add(myFavSeasonImageArray[i]);
+        //if loading app for very first time
+        if(savedInstanceState==null) {
+            //put data of arrays into arraylists
+            season_names_AL = new ArrayList<>(Arrays.asList(myFavSeasonArray));
+            season_images_AL = new ArrayList<>();
+            for (int i = 0; i < myFavSeasonImageArray.length; i++) {
+                season_images_AL.add(myFavSeasonImageArray[i]);
+            }
+        }else{
+            loadSaveData(this);
+//            loadSaveData();
         }
-
 
         //array adapter for displaying list items using fixed layout
 //        adapter=new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,
@@ -65,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         //custom adapter for arrays data
 //        myOwnAdapter=new CustomAdapter(MainActivity.this, myFavSeasonArray,myFavSeasonImageArray);
         //custom adapter for array list data
-        myOwnAdapter=new CustomAdapter(MainActivity.this, season_names_AL,season_images_AL);
+        myOwnAdapter = new CustomAdapter(MainActivity.this, season_names_AL, season_images_AL);
 
 
         //set adapater for listview
@@ -76,9 +85,9 @@ public class MainActivity extends AppCompatActivity {
         myFavSeasonList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String season_name=myFavSeasonList.getItemAtPosition(position).toString();
-                Float season_rate_value=((RatingBar)view.findViewById(R.id.season_rate)).getRating();
-                Intent intent=new Intent(MainActivity.this, DetailsActivity.class);
+                String season_name = myFavSeasonList.getItemAtPosition(position).toString();
+                Float season_rate_value = ((RatingBar) view.findViewById(R.id.season_rate)).getRating();
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
                 intent.putExtra("season_name", season_name);
                 intent.putExtra("season_rating", season_rate_value);
                 startActivity(intent);
@@ -88,13 +97,85 @@ public class MainActivity extends AppCompatActivity {
         //remove list row data
         myFavSeasonList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-               season_names_AL.remove(position);
-                season_images_AL.remove(position);
-                myOwnAdapter.notifyDataSetChanged();
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                String seasonNameToDelete = myFavSeasonList.getItemAtPosition(position).toString();
+                final AlertDialog.Builder alertBox = new AlertDialog.Builder(MainActivity.this);
+                alertBox.setTitle("Do You want to Delete this: " + seasonNameToDelete);
+                //right side button
+                alertBox.setPositiveButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                //left side button
+                alertBox.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        which = position;
+//                season_names_AL.remove(position);
+//                season_images_AL.remove(position);
+                        season_names_AL.remove(which);
+                        season_images_AL.remove(which);
+                        myOwnAdapter.notifyDataSetChanged();
+                    }
+                });
 
-                return false;
+                AlertDialog dialog = alertBox.create();
+                dialog.show();
+                return true;
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        saveSeasonData();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        saveSeasonData();
+        super.onStop();
+    }
+    //using shared preferences to store data in "mySeasonData" file
+    public boolean saveSeasonData(){
+        //1. Shared preferences option choice
+        SharedPreferences sharedPreferences=getSharedPreferences("mySeasonData",MODE_PRIVATE);
+        //editor to insert data in shared preferences
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putInt("seasonNameALSize",season_names_AL.size());
+        editor.putInt("seasonImageALSize", season_images_AL.size());
+
+        for(int i=0; i < season_names_AL.size(); i++){
+            //editor adds by default key of each data. Remove that for our convenience
+            editor.remove("seasonName"+i);
+            editor.putString("seasonName", season_names_AL.get(i));
+        }
+
+        for(int i=0; i < season_images_AL.size(); i++){
+            editor.remove("seasonImage"+i);
+            editor.putInt("seasonImage", season_images_AL.get(i));
+        }
+
+        //commit or apply the input to shared preferences
+
+        return editor.commit();
+    }
+
+    public void loadSaveData(Context mContext){
+        SharedPreferences sharedPreferences= PreferenceManager.getDefaultSharedPreferences(mContext);
+        //retrieve size of array lists for displaying data
+        int sharedSeasonNameSize=sharedPreferences.getInt("seasonNameALSize",0);
+        int sharedSeasonImageSize=sharedPreferences.getInt("seasonImageALSize",0);
+
+        for(int i=0; i<sharedSeasonNameSize; i++){
+            season_names_AL.add(sharedPreferences.getString("seasonName"+i,null));
+        }
+
+        for(int i=0; i<sharedSeasonImageSize; i++){
+            season_images_AL.add(sharedPreferences.getInt("seasonImage"+i,0));
+        }
     }
 }
